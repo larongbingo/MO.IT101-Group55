9b12c3bd6711febd747a47ba05466073cb120154
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-03-28 09:43:25 +0800

    chore: add mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-core:5.23.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.23.0")
     implementation(kotlin("stdlib-jdk8"))
     implementation("de.siegmar:fastcsv:4.1.1")
 }

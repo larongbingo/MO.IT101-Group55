d41da4291e62077382675636a75cab4134dffafe
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-04-09 00:36:16 +0800

    feat: add parse method in EmploymentStatus

--- motorph-payroll-core/src/org/motorph/employees/EmploymentStatus.java
+++ motorph-payroll-core/src/org/motorph/employees/EmploymentStatus.java
@@ -1,7 +1,20 @@
 package org.motorph.employees;
 
+import org.motorph.core.MotorPhException;
+import org.motorph.core.results.Result;
+
 public enum EmploymentStatus
 {
     Probationary,
     Regular;
+
+    public static Result<EmploymentStatus> parse(String input) {
+        var statuses = EmploymentStatus.values();
+        for (var status : statuses) {
+            if (status.name().equalsIgnoreCase(input)) {
+                return Result.success(status);
+            }
+        }
+        return Result.failure(new MotorPhException("Invalid EmploymentStatus String"));
+    }
 }

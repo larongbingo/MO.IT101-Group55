0b5b248ce8cd84f66f58f14668f5f21b7f05fee7
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-04-09 00:32:20 +0800

    feat: add isSuccess default impl

--- motorph-payroll-core/src/org/motorph/core/results/Result.java
+++ motorph-payroll-core/src/org/motorph/core/results/Result.java
@@ -3,6 +3,10 @@ package org.motorph.core.results;
 import org.motorph.core.MotorPhException;
 
 public sealed interface Result<T> permits Success, Failure {
+    public default boolean isSuccess() {
+        return this instanceof Success<T>;
+    }
+
     static <X> Success<X> success(X value) {
         return new Success<>(value);
     }

33a607d402349d61399734aaea6d133d7d0e59a9
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-04-18 18:34:20 +0800

    feat(jvm): add minimum window size and title

--- motorph-payroll-app/jvm-app/src/main.kt
+++ motorph-payroll-app/jvm-app/src/main.kt
@@ -4,11 +4,14 @@ import androidx.compose.ui.window.application
 import org.motorph.AppRouter
 import org.motorph.data.loading.loadData
 import org.motorph.initKoin
+import java.awt.Dimension
 
 fun main() = application {
     initKoin()
     loadData()
     Window(onCloseRequest = ::exitApplication) {
+        window.minimumSize = Dimension(600, 800)
+        window.title = "MotorPh Payroll"
         MaterialTheme {
             AppRouter()
         }
